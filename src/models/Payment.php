9f934f671ba8a000829384b5699dd759c4a1d311
Orkida Loku <orkida027@gmail.com>
2025-01-17 00:41:47 +0100

    configured the backend of payment , needs to be fixed again

--- src/models/Payment.php
+++ src/models/Payment.php
@@ -1,1 +1,23 @@
 <?php
+
+require_once 'C:\xampp\htdocs\web-project\config\paymentsConfig.php'
+
+?>
+
+<script src="https://js.stripe.com/v3/"></script>
+<script src="/public/js/checkout.js" STRIPE_PUBLISHABLE_KEY="<?php echo STRIPE_PUBLISHABLE_KEY; ?>" defer></script>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Payment</title>
+</head>
+<body>
+<h1>Premium Subscription</h1>
+<p>Subscribe for just €25 and enjoy exclusive features!</p>
+<button id="checkout-button">Pay Now</button>
+</body>
+</html>
+
