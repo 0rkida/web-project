a5b07c5b6e5372be1f72e533b00fe90450a4636e
Orkida Loku <orkida027@gmail.com>
2025-01-16 22:32:05 +0100

    Added the payment configuration

---
+++ config/paymentsConfig.php
@@ -0,0 +1,16 @@
+<?php
+
+$itemName = "Premium Subscription" ;
+$itemPrice = "25";
+$currency = "EUR" ;
+
+define('STRIPE_PUBLISHABLE_KEY', 'sk_test_51QdZaOIA6j8Agjdo5GJTLy0SpIDPNae1bPpzug7GTqcDIUlNR7CKK1ojTH9gGYRe0uEHUjbnpIDGSKukUVTONETg00wfXvfRpk' );
+define('STRIPE_SECRET_KEY' , 'pk_test_51QdZaOIA6j8AgjdoONN2YmHKTojcogE82ZcF8ntm0l1YwdZNUKNnlDgxb62vZ7IBVbS1NfyGQoRNxjWn6o0bvJxE00alHhiENc');
+
+define('DB_HOST', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_NAME', 'datting_app');
+
+?>
+
