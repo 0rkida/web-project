eb9c4326abba43401b4d52790d63425de95b12b5
holtaozuni666 <Holta.ozuni@fti.edu.al>
2025-01-21 17:56:07 +0100

    Fixed failed attempts and 15 min inactivity

--- public/profile/view_profile.php
+++ public/profile/view_profile.php
@@ -36,6 +36,7 @@ $conn->close();
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>View Profile</title>
+    <script src="/js/logout.js"></script>
 </head>
 <body>
 
